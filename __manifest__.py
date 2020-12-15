fc66b0a3fdae9c3b2051bf538438bde4f95d0288
German Chiocchia <germanchiocchia@gmail.com>
2020-12-15 11:09:20 -0300

    beginning

---
+++ __manifest__.py
@@ -0,0 +1,43 @@
+{
+    'name': 'kinesiology',
+    'summary': 'kinesiology Module Project',
+    'version': '1.0',
+
+    'description': """
+kinesiology Module Project.
+==============================================
+
+
+    """,
+
+    'author': 'Adrian Borella',
+    'website': 'https://github.com/adriannborella',
+    'license': 'AGPL-3',
+    
+    'category': 'Uncategorized',
+
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
+    'data': [
+        
+    ],
+    'demo': [
+    ],
+    'js': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+    ],
+    'images': [
+    ],
+    'test': [
+    ],
+
+    'installable': True
+}
